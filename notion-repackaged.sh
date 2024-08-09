37a250d174885b29bb83863112ce730a799be153
Stephen <sdkane@users.noreply.github.com>
2024-08-09 19:27:48 -0400

    3.12.1

--- notion-repackaged.sh
+++ notion-repackaged.sh
@@ -4,13 +4,13 @@
 #
 
 # Version of the original Notion App installer to repackage
-export NOTION_VERSION=3.0.0
+export NOTION_VERSION=3.12.1
 
 # Revision of the current version
 export NOTION_REPACKAGED_REVISION=1
 
 # The md5sum hash of the downloaded .exe for the installer
-export NOTION_DOWNLOAD_HASH=a0d687989346bc1b16d620a0af0f3642
+export NOTION_DOWNLOAD_HASH=7faf8166c81658d1aa8ede4198e1ab3b
 
 # The commit of notion-enhancer/desktop to target
 export NOTION_ENHANCER_DESKTOP_COMMIT=a88c45cc80f0cfd83f205ddfbbca695f50db16ef
