c9d28cc454f4faa68b0dcaf50e726db250627ab9
Stephen <sdkane@users.noreply.github.com>
2024-08-15 13:44:39 -0400

    notion 3.12.2
    updating soon to be deprecated actions in workflow

--- notion-repackaged.sh
+++ notion-repackaged.sh
@@ -4,13 +4,13 @@
 #
 
 # Version of the original Notion App installer to repackage
-export NOTION_VERSION=3.12.1
+export NOTION_VERSION=3.12.2
 
 # Revision of the current version
 export NOTION_REPACKAGED_REVISION=1
 
 # The md5sum hash of the downloaded .exe for the installer
-export NOTION_DOWNLOAD_HASH=7faf8166c81658d1aa8ede4198e1ab3b
+export NOTION_DOWNLOAD_HASH=67c047a826e6e13b530bd1e91b0bb752
 
 # not supported for 3.x.x yet
 # export NOTION_REPACKAGED_AIO_SKIP_ENHANCED=true
